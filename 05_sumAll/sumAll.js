bfc225353c082e68914b6856a3808b293c814835
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-14 18:57:53 -0300

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
 const sumAll = function() {
 
+    let start = arguments[0];
+    let end = arguments[1];
+
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
+
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        let aux = start;
+        start = end;
+        end = aux;
+    }
+
+    let result = 0;
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
