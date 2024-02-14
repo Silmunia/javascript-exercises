75d3bc928818eb62ff7d7d557499e739c3a8f0ec
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-14 19:14:29 -0300

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let newTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+  let result = Math.round(newTemp * 10) / 10;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let newTemp = (celsiusTemp * 9 / 5) + 32;
+
+  let result = Math.round(newTemp * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
