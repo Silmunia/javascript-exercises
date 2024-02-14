c2bb259ed084dc059a474f303aa2207cc98ee26c
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-14 18:42:57 -0300

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    let result = [];
 
+    for (let i = 0; i < arguments[0].length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (arguments[0][i] === arguments[j]) {
+                break;
+            } else if (j === arguments.length-1) {
+                result.push(arguments[0][i]);
+            }
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
