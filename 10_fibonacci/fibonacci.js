aea7c2ce296390e52c4f2628f212e383e238a7fe
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-16 09:05:24 -0300

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    const input = Number(index);
+
+    if (input < 0) {
+        return 'OOPS';
+    } else if (input === 0) {
+        return 0;
+    }
+
+    let array = [1, 1];
+
+    for (let i = array.length-1; i < input-1; i++) {
+        let newValue = array[i] + array[i-1];
+
+        array.push(newValue);
+    }
+
+    return array[array.length-1];
 };
 
 // Do not edit below this line
