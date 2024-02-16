fe885c81f7a06ab0358a3ac7877cc820f7bab0bf
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-16 08:25:44 -0300

    Complete exercises 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const cleanString = string
+        .split(',').join('')
+        .split('!').join('')
+        .split('.').join('')
+        .split(' ').join('')
+        .toLowerCase();
+    
+    const reverseString = cleanString.split('').reverse().join('');
+
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
