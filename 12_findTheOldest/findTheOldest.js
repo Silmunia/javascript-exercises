c7203d34ad9a0a580c3594e39f9b402401facfbc
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-16 21:18:08 -0300

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((acc, item) => {
+        let accAge = (acc['yearOfDeath'] ? acc['yearOfDeath'] : new Date().getFullYear()) - acc['yearOfBirth']
 
+        let itemAge = (item['yearOfDeath'] ? item['yearOfDeath'] : new Date().getFullYear()) - item['yearOfBirth']
+
+        if (itemAge > accAge) {
+            acc = item;
+        }
+
+        return acc;
+    });
 };
 
 // Do not edit below this line
